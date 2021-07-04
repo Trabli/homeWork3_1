1cb545dbd3a5880c4db37c012b0e8623728f8b0e
Aleksander Bobryshev <62813365+Trabli@users.noreply.github.com>
2021-07-04 22:43:58 +0600

    Update Program.cs

--- homeWork3/Program.cs
+++ homeWork3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace homeWork3
 {
@@ -6,16 +6,16 @@ namespace homeWork3
     {
         static void Main(string[] args)
         {
-            int images = 52;
-            int row;
+            int imagesQuantity = 52;
+            int rowOfImages;
             int extraImages;
 
-            row = images / 3;
-            extraImages = images % 3;
+            rowOfImages = imagesQuantity / 3;
+            extraImages = imagesQuantity % 3;
 
             Console.WriteLine("У пользователя альбом с 52-мя картинками.");
             Console.WriteLine("Их нужно вывести на экран по 3 в ряд, и узнать остаток картинок.");
-            Console.WriteLine("Картинки можно вывести в " + row + " рядов");
+            Console.WriteLine("Картинки можно вывести в " + rowOfImages + " рядов");
             Console.WriteLine(extraImages + " картинка не влезет в ряды и будет в остатке");
 
         }
