f16679163a423e16e066c33c201f97a1ea927ae7
Aleksander Bobryshev <62813365+Trabli@users.noreply.github.com>
2021-07-05 00:13:54 +0600

    Update Program.cs

--- homeWork3/Program.cs
+++ homeWork3/Program.cs
@@ -12,8 +12,8 @@ namespace homeWork3
             int row_WithImages = images_InAlbum / images_inOneRow;
             int imagesLeft = images_InAlbum % images_inOneRow;
 
-            Console.WriteLine("У пользователя альбом с 52-мя картинками.");
-            Console.WriteLine("Их нужно вывести на экран по 3 в ряд, и узнать остаток картинок.");
+            Console.WriteLine("У пользователя альбом с " + images_InAlbum + "-мя картинками.");
+            Console.WriteLine("Их нужно вывести на экран по " + images_inOneRow + " в ряд, и узнать остаток картинок.");
             Console.WriteLine("Картинки можно вывести в " + row_WithImages + " рядов");
             Console.WriteLine(imagesLeft + " картинка не влезет в ряды и будет в остатке");
         }
